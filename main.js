8c2393de48b048e8d8c4ee263ba2cd67e8449c53
DimaLite <80548053+Dimalite@users.noreply.github.com>
2022-10-19 22:59:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -102,3 +102,77 @@ fetch(url).then((response) => response.text())
 //     }
     
 //     getUser();
+
+This is another file!
+      import React from 'react'
+import Axios from 'axios';
+
+const serverUrl = 'http://localhost:3030/index.php?date=';
+const url = 'http://technik300.ml/?i=1'
+
+const path1 = 'http://localhost:3030/index.php'
+const path2 = 'http://localhost:3030/body.php'
+
+
+// fetch(url, {method: 'GET',  mode: 'cors',
+// headers: {
+//     'Content-Type': 'application/json',
+// }})
+//   .then((response) => response.text())
+//   .then((data) => {
+//     console.log('Success:', data);
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+// ==============================
+// async function getUser() {
+//   try {
+//     const response = await fetch('http://technik300.ml/?i=1', {
+//       method: 'GET',
+//       headers: {
+//         accept: 'application/json',
+//       },
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`Error! status: ${response.status}`);
+//     }
+
+//     const result = await response.text();
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// getUser();
+
+// async function getMyDate(){  
+//   fetch(url, {method: 'GET',  mode: 'cors',
+//   headers: {
+//       'Content-Type': 'application/json',
+//   }})
+//   .then((response) => response.text())
+//   .then((data) => {
+//     console.log('Success:', data);
+//   })
+  
+// }
+// getMyDate()
+
+// fetch(url)
+//   .then(response => response.text())
+//   .then(commits => console.log(commits));
+
+// "http://technik300.ml/?i=1"
+function DataServer() {
+  Axios.get(path1)
+ 
+  .then(data => console.log(data))
+  return (
+    <div>DataServer</div>
+  )
+}
+
+export default DataServer;
